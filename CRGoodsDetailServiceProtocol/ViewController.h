9175ddcd70c24d2044a2d003ee380148d9db695a
许磊 <lxu@jinhetech.com>
2018-02-08 13:47:11 +0800

    Initial Commit

---
+++ CRGoodsDetailServiceProtocol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CRGoodsDetailServiceProtocol
+//
+//  Created by 许磊 on 2018/2/8.
+//  Copyright © 2018年 Jinhetech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CRGoodsDetailServiceProtocol/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CRGoodsDetailServiceProtocol
+//
+//  Created by 许磊 on 2018/2/8.
+//  Copyright © 2018年 Jinhetech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
